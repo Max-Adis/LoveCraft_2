0dbd3723b864c1f64e2f70feb2145ed5636fff5b
Max_Adis <maxhounkpatin001@gmail.com>
2025-12-04 21:15:05 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,40 +1,46 @@
-// Firebase configuration - MODIFIÉ POUR FIRESTORE
+// Firebase configuration
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { 
-    getFirestore, 
-    collection, 
-    addDoc, 
-    getDoc, 
-    doc, 
-    updateDoc,
-    increment 
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
+    getDatabase, 
+    ref, 
+    set, 
+    get, 
+    update,
+    remove,
+    query,
+    orderByChild,
+    equalTo
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
-// TES CLÉS FIREBASE (celles que tu as partagées)
 const firebaseConfig = {
-  apiKey: "AIzaSyB1hcyt4IVDtKcOw2JcVnCcLP5gOvPt4F0",
-  authDomain: "lovecraft-web.firebaseapp.com",
-  projectId: "lovecraft-web",
-  storageBucket: "lovecraft-web.firebasestorage.app",
-  messagingSenderId: "28643241616",
-  appId: "1:28643241616:web:616f5aa143d739cd0f3215",
-  measurementId: "G-L1N4GTS1EL"
+    apiKey: "AIzaSyB1hcyt4IVDtKcOw2JcVnCcLP5gOvPt4F0",
+    authDomain: "lovecraft-web.firebaseapp.com",
+    databaseURL: "https://lovecraft-web-default-rtdb.firebaseio.com",
+    projectId: "lovecraft-web",
+    storageBucket: "lovecraft-web.firebasestorage.app",
+    messagingSenderId: "28643241616",
+    appId: "1:28643241616:web:616f5aa143d739cd0f3215",
+    measurementId: "G-L1N4GTS1EL"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
+const database = getDatabase(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-// Export des fonctions Firebase
 export { 
-    db, 
-    collection, 
-    addDoc, 
-    getDoc, 
-    doc, 
-    updateDoc, 
-    increment,
-    analytics 
+    database, auth, googleProvider,
+    ref, set, get, update, remove, query, orderByChild, equalTo,
+    createUserWithEmailAndPassword, signInWithEmailAndPassword,
+    signInWithPopup, signOut, onAuthStateChanged
 };
